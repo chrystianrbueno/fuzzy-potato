943defda6a748755c14dfdb62ddfde429f9410db
Eduardo Gautier <duduchn2011@gmail.com>
2021-08-19 16:31:23 -0300

    #4

--- src/main/java/com/example/accessingdatajpa/CustomerRestController.java
+++ src/main/java/com/example/accessingdatajpa/CustomerRestController.java
@@ -1,8 +1,11 @@
 package com.example.accessingdatajpa;
 
+import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,8 +15,11 @@ public class CustomerRestController {
 	private CustomerRepository repository;
 
 	@GetMapping("/api/customers")
-	public Iterable<Customer> getCustomers() {
-		Iterable<Customer> lista = repository.findAll(); 
-		return lista; 
+	public ResponseEntity<Collection<Customer>> getCustomers() {
+		Collection<Customer> lista = repository.findAll(); 
+		if(lista.isEmpty()){
+			return new ResponseEntity<Collection<Customer>>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<Collection<Customer>>(lista, HttpStatus.OK);
 	}	
 }
