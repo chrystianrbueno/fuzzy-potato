a184088a68aeb45b5d2a581b1938500fedc63adc
Chrystian Rocha <chrystianrbueno@gmail.com>
2021-08-19 15:34:43 -0300

    adicionado teste na classe CustomerRestControllerTest

---
+++ src/test/java/com/example/accessingdatajpa/CustomerRestControllerTest.java
@@ -0,0 +1,33 @@
+package com.example.accessingdatajpa;
+
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class CustomerRestControllerTest {
+
+	@Autowired
+	private MockMvc mvc;
+
+	@Test
+	void testGetAllCustomersSuccess() throws Exception {
+		mvc.perform(MockMvcRequestBuilders
+				.get("/api/customers")
+					.accept(MediaType.APPLICATION_JSON))
+					.andExpect(status().isOk())
+					.andExpect(content().contentType("application/json"))
+					.andExpect(jsonPath("$.[0].id").value(1))
+					.andExpect(jsonPath("$.[0].firstName").value("Jack"))
+					.andExpect(jsonPath("$.[0].lastName").value("Bauer"));
+	}
+
+}
